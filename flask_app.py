cb85e53338e4da10b356058b66415b48fc062077
bobchew93 <bobchew93@gmail.com>
2025-02-09 16:12:18 +0800

    Add basic template and styling

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load environment variables from .env file
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Welcome to your project workspace!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
